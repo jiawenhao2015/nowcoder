0d375ed57fe16fa8ba493f7f49360806f53448f1
jiawenhao2015 <1490776271@qq.com>
2017-02-17 18:51:35 +0800

    回溯法判断矩阵中是否包含路径

--- nowcoder/nowcoder/jianzhioffer.cpp
+++ nowcoder/nowcoder/jianzhioffer.cpp
@@ -37,11 +37,63 @@ struct TreeLinkNode {
 	struct TreeLinkNode *right;
 	struct TreeLinkNode *next;
 	TreeLinkNode(int x) :val(x), left(NULL), right(NULL), next(NULL) {
-
 	}
 };
 class Solution {
 public:
+
+	/*请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。路径可以从矩阵中的任意一个格子开始，
+	每一步可以在矩阵中向左，向右，向上，向下移动一个格子。如果一条路径经过了矩阵中的某一个格子，则该路径不能再进入该格子。
+	例如 a b c e s f c s a d e e 矩阵中包含一条字符串"bccced"的路径，
+	但是矩阵中不包含"abcb"路径，因为字符串的第一个字符b占据了矩阵中的第一行第二个格子之后，路径不能再次进入该格子。*/	
+	/*这是一个可以用回朔法解决的典型题。http://www.voidcn.com/blog/huzhigenlaohu/article/p-6090898.html
+	首先，在矩阵中任选一个格子作为路径的起点。如果路径上的第i个字符不是ch，那么这个格子不可能处在路径上的第i个位置。
+	如果路径上的第i个字符正好是ch，那么往相邻的格子寻找路径上的第i+1个字符。除在矩阵边界上的格子之外，其他格子都有4个相邻的格子。
+	重复这个过程直到路径上的所有字符都在矩阵中找到相应的位置。
+	由于回朔法的递归特性，路径可以被开成一个栈。当在矩阵中定位了路径中前n个字符的位置之后，
+	在与第n个字符对应的格子的周围都没有找到第n+1个字符，这个时候只要在路径上回到第n-1个字符，重新定位第n个字符。
+	由于路径不能重复进入矩阵的格子，还需要定义和字符矩阵大小一样的布尔值矩阵，用来标识路径是否已经进入每个格子。
+	当矩阵中坐标为（row,col）的格子和路径字符串中相应的字符一样时，从4个相邻的格子(row,col-1),(row-1,col),(row,col+1)
+	以及(row+1,col)中去定位路径字符串中下一个字符如果4个相邻的格子都没有匹配字符串中下一个的字符，
+	表明当前路径字符串中字符在矩阵中的定位不正确，我们需要回到前一个，然后重新定位。
+	一直重复这个过程，直到路径字符串上所有字符都在矩阵中找到合适的位置*/
+	bool isHasPath(char* matrix,int rows,int cols,int i,int j,char* str,int k,bool* flag)
+	{
+		int index = i*cols + j;
+		if (i < 0 || i >= rows || j < 0 || j >= cols || matrix[index] != str[k] || flag[index]) return false;
+
+		if (k == strlen(str) - 1) return true;
+		
+		flag[index] = true;
+
+		if (isHasPath(matrix, rows, cols, i, j + 1, str, k + 1, flag) ||
+			isHasPath(matrix, rows, cols, i, j - 1, str, k + 1, flag) ||
+			isHasPath(matrix, rows, cols, i + 1, j, str, k + 1, flag) ||
+			isHasPath(matrix, rows, cols, i - 1, j, str, k + 1, flag) )			
+		{
+			return true;
+		}
+		flag[index] = false;
+		return false;
+	}
+	bool hasPath(char* matrix, int rows, int cols, char* str)
+	{
+		if (strlen(matrix)<rows*cols || strlen(str)>strlen(matrix)) return false;
+
+		bool* flag = new bool[rows*cols];//标记该位置是否有效		
+		for (int i = 0; i < rows*cols; i++)flag[i] = false;		
+		for (int i = 0; i < rows;i++)
+		{ 
+			for (int j = 0; j < cols;j++)
+			{
+				if (isHasPath(matrix, rows, cols, i, j, str,0, flag))
+					return true;
+			}			
+		}
+
+		delete[] flag;
+		return false;
+	}
 	/*输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
 	假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
 	例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。*/
@@ -1849,9 +1901,7 @@ int main()
 	popV.push_back(5);
 	popV.push_back(3);
 	popV.push_back(8);
-	popV.push_back(6);
-	
-	
+	popV.push_back(6);	
 
 
 	Solution sl;
@@ -1859,14 +1909,13 @@ int main()
 	vector<TreeNode*> tree;
 	
 	TreeNode * root;
-	root =  sl.reConstructBinaryTree(test,popV);
-	sl.InOrderTraverse(root, tree);
+
+	cout << sl.hasPath("abcesfcsadee",3,4,"aced");
 	
 	for (int i = 0; i < tree.size();i++)
 	{		
-		cout << tree[i]->val << " ";		
-	}
-	
+		cout << tree[i]->val << " ";
+	}	
 
 	getchar();
 	return 0;
