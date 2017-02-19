54716d22f4bee1aa17b7ef3f359eb0c218e91a59
jiawenhao2015 <1490776271@qq.com>
2017-02-19 18:49:55 +0800

    新增LeetCode练习

---
+++ nowcoder/leetcode/leetcode.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+struct TreeNode{
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+		val(x), left(NULL), right(NULL) {
+	}
+};
+class Solution 
+{
+public:
+	/*Given a binary tree, find its minimum depth.The minimum depth is the number of nodes
+	along the shortest path from the root node down to the nearest leaf node.*/
+	int minDepth(TreeNode *root)
+	{
+		if(root == NULL) return 0;		
+		if (root->left==NULL) return 1 + minDepth(root->right);
+		if (root->right == NULL) return 1 + minDepth(root->left);
+		return 1 + min(minDepth(root->left), minDepth(root->right));		
+	}
+
+	
+};
+
+int main()
+{
+
+	return 0;
+}
