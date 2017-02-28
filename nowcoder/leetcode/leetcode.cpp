a847ec819352654d7d7179391a17d0de5a9d12ef
jiawenhao2015 <1490776271@qq.com>
2017-02-28 23:29:26 +0800

    canCompleteCircuit2

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -33,6 +33,30 @@ public:
 	Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
 	Note:
 	The solution is guaranteed to be unique.*/
+	int canCompleteCircuit2(vector<int> &gas, vector<int> &cost) {
+		int i, j, n = gas.size();
+
+		/*
+		* If start from i, stop before station x -> no station k from i + 1 to x - 1 can reach x.
+		* Bcoz if so, i can reach k and k can reach x, then i reaches x. Contradiction.
+		* Thus i can jump directly to x instead of i + 1, bringing complexity from O(n^2) to O(n).
+		*/
+		// start from station i
+		for (i = 0; i < n; i += j) {
+			int gas_left = 0;
+			// forward j stations
+			for (j = 1; j <= n; j++) {
+				int k = (i + j - 1) % n;
+				gas_left += gas[k] - cost[k];
+				if (gas_left < 0)
+					break;
+			}
+			if (j > n)
+				return i;
+		}
+
+		return -1;
+	}
 	int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
 	{
 		if (gas.size() != cost.size() || gas.size() == 0)return -1;
