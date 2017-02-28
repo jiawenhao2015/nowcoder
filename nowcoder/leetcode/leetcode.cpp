ab34221adbeadb4268d9c71fa5aadaf92cfcfb40
jiawenhao2015 <1490776271@qq.com>
2017-02-28 23:28:27 +0800

    leetcode-134-Gas Station

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -26,6 +26,38 @@ struct Point {
 class Solution 
 {
 public:
+	/*leetcode-134-Gas Station
+	There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+	You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). 
+	You begin the journey with an empty tank at one of the gas stations.
+	Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+	Note:
+	The solution is guaranteed to be unique.*/
+	int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
+	{
+		if (gas.size() != cost.size() || gas.size() == 0)return -1;
+
+		int tank = 0;
+		int num = gas.size();
+		for (int i = 0; i < num;i++)
+		{
+			tank = 0;
+			for (int j = i; j < num + i;j++)
+			{
+				tank += gas[j%num];
+				tank -= cost[j%num];
+				if (tank < 0)//不能到下一个station 重新开始
+				{			
+					j = num+i;	//退出内层循环
+				}
+			}
+			if (tank>=0)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
 	/*	leetcode-135-Candy
 	There are N children standing in a line. Each child is assigned a rating value.
 	You are giving candies to these children subjected to the following requirements:
@@ -50,7 +82,7 @@ public:
 			}
 		}
 		sum = candyvec[ratings.size() - 1];
-		for (int i = ratings.size() - 2; i >=0 ;i--)
+		for (int i = ratings.size() - 2; i >=0 ;i--)//反向遍历一遍 
 		{
 			if ((ratings[i] > ratings[i + 1]) && candyvec[i] < (candyvec[i + 1] + 1))candyvec[i] = candyvec[i + 1] + 1;
 			sum += candyvec[i];
