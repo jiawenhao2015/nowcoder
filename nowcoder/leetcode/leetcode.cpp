ed77a4a9341fa235dd76a443d1dc22dfd0cf8d80
jiawenhao2015 <1490776271@qq.com>
2017-03-02 12:57:58 +0800

    leetcode-121-Best Time to Buy and Sell Stock

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -27,6 +27,7 @@ struct Point {
 class Solution 
 {
 public:
+
 	/*leetcode-121-Best Time to Buy and Sell Stock
 	Say you have an array for which the ith element is the price of a given stock on day i.
 	If you were only permitted to complete at most one transaction 
