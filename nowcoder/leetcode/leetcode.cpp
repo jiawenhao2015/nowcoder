61836d498ed9f0b7ee6e53f2f94290d099b96906
jiawenhao2015 <1490776271@qq.com>
2017-03-27 22:47:33 +0800

    leetcode-21-Merge Two Sorted Lists.txt

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -56,6 +56,38 @@ public:
 	}
 
 	/*leetcode[115] Distinct Subsequences*/
+
+	/*leetcode-21-Merge Two Sorted Lists
+	Merge two sorted linked lists and return it as a new list.
+	The new list should be made by splicing together the nodes of the first two lists.*/
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
+	{
+		ListNode* dummy = new ListNode(0);
+		ListNode* p = dummy ;
+		while (l1 != NULL && l2 != NULL)
+		{
+			if (l1->val < l2->val)
+			{
+				p->next = l1;
+				l1 = l1->next;
+			}
+			else
+			{
+				p->next = l2;
+				l2 = l2->next;
+			}
+			p = p->next;			 
+		}
+		if (l1!=NULL)
+		{
+			p->next = l1;
+		}
+		if (l2!= NULL)
+		{
+			p->next = l2;
+		}
+		return dummy->next;
+	}
 	/*leetcode-109-Convert Sorted List to Binary Search Tree
 	Given a singly linked list where elements are sorted in
 	ascending order, convert it to a height balanced BST.*/
