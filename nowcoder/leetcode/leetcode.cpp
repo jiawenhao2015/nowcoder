079bea857eb2bb39bfc9a4e6a692a077bc953ff5
jiawenhao <1490776271@qq.com>
2017-04-04 23:48:04 +0800

    add 118 pascal

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -2393,6 +2393,38 @@ int removeDuplicates(int A[], int n)
         }
         return cnt;
     }
+/*leetcode-118-Pascal's Triangle
+Given numRows, generate the first numRows of Pascal's triangle.
+For example, given numRows = 5,
+Return
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+*/
+vector<vector<int> > generate(int numRows)
+{
+        vector<vector<int>> result;
+        if(numRows<1)return result;
+        vector<int>level;
+        level.push_back(1);
+        result.push_back(level);
+        for(int i = 1;i<numRows;i++)
+        {
+            level.clear();
+            level.push_back(1);
+            for(int j=1;j<i;j++)
+            {
+                level.push_back(result[i-1][j-1] + result[i-1][j]);
+            }
+            level.push_back(1);
+            result.push_back(level);
+        }
+        return result;
+}
 
 
 };
