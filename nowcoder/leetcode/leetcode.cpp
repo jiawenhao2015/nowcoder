89f42d2dbba09e78d47e8b52d6e6a86dd4f05713
jiawenhao <1490776271@qq.com>
2017-04-08 12:37:33 +0800

    add 92 exercise

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -2615,6 +2615,46 @@ public:
 			return deleteDuplicates(p);
 		}
 	}
+/*leetcode-92-Reverse Linked List II
+Reverse a linked list from position m to n. Do it in-place and in one-pass.
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+return 1->4->3->2->5->NULL.
+Note:
+Given m, n satisfy the following condition:
+1 ≤ m ≤ n ≤ length of list.
+思路：
+*/
+ListNode* reverseBetween(ListNode* head, int m, int n)
+ {
+     ListNode dumb(0);
+     dumb.next = head;
+     if(m == n) return head;
+     ListNode* preM = &dumb;//保存m之前结点
+     ListNode* pM = head,*pN = head ,*temp = head;
+     int index=1;
+     while(pN != NULL)
+     {
+         if(index < m)
+         {
+             index++;
+             preM = pM;
+             pM = pM->next;
+             pN = pN->next;
+         }
+         else if(index >= m && index <= n)
+         {
+             temp = pN->next;
+             pN->next = preM->next;
+             preM->next =pN;
+             pN = temp;
+             index++;
+         }
+         else if(index > n)        break;
+     }
+        pM->next = pN;
+   return dumb.next;
+}
 };
 
 int main()
