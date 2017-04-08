fdd3dfa81459d8e07ce00827c3ce6fa73ee81ad7
jiawenhao <1490776271@qq.com>
2017-04-08 12:39:09 +0800

    add 92 new solution

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -2655,6 +2655,23 @@ ListNode* reverseBetween(ListNode* head, int m, int n)
         pM->next = pN;
    return dumb.next;
 }
+ListNode *reverseBetween2(ListNode *head, int m, int n) {
+    if(m==n)return head;
+	n-=m;
+    ListNode prehead(0);
+    prehead.next=head;
+    ListNode* pre=&prehead;
+    while(--m)pre=pre->next;
+    ListNode* pstart=pre->next;
+    while(n--)
+    {
+        ListNode *p=pstart->next;
+        pstart->next=p->next;
+        p->next=pre->next;
+        pre->next=p;
+    }
+    return prehead.next;
+}
 };
 
 int main()
