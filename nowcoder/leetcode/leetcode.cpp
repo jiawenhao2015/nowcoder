64870b968a075393f1febeb00d4b2370cf22a3ac
jiawenhao2015 <1490776271@qq.com>
2017-05-12 20:20:49 +0800

    leetcode-442-Find All Duplicates in an Array.txt

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -3553,7 +3553,8 @@ public:
 	 [4,3,2,7,8,2,3,1]
 	 Output:
 	 [5,6]*/
-	 vector<int> findDisappearedNumbers(vector<int>& nums) {
+	 vector<int> findDisappearedNumbers(vector<int>& nums)
+	 {
 		 int len = nums.size();
 		 for (int i = 0; i<len; i++) {
 			 int m = abs(nums[i]) - 1; // index start from 0
@@ -3565,7 +3566,32 @@ public:
 		 }
 		 return res;
 	 }
-
+	 /*leetcode-442-Find All Duplicates in an Array
+	 Given an array of integers, 1 ¡Ü a[i] ¡Ü n (n = size of array), some elements appear twice and others appear once.
+	 Find all the elements that appear twice in this array.
+	 Could you do it without extra space and in O(n) runtime?
+	 Example:
+	 Input:
+	 [4,3,2,7,8,2,3,1]
+	 Output:
+	 [2,3]*/
+	 vector<int> findDuplicates(vector<int>& nums)
+	 {
+		 vector<int>ret;
+		 for (int i = 0; i < nums.size();i++)
+		 {
+			 int index = abs(nums[i]) - 1;
+			 if (nums[index] > 0)
+			 {
+				 nums[index] = - nums[index];
+			 }
+			 else
+			 {
+				 ret.push_back(abs(nums[i]));
+			 }
+		 }
+		 return ret;
+	 }
 };
 
 int main()
