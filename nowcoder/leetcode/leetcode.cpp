b3477eeea02281d4eb9fb8e99d35e1204d339c59
jiawenhao2015 <1490776271@qq.com>
2017-05-26 10:18:42 +0800

    leetcode-12-Integer to Roman.txt

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -4179,8 +4179,18 @@ public:
 			 default:;
 			 }			  
 		 }
-		 return st.empty();
-		
+		 return st.empty();		
+	 }
+	 /*leetcode-12-Integer to Roman
+	 Given an integer, convert it to a roman numeral.
+	 */
+	 string intToRoman(int num)
+	 {
+		 string M[] = { "", "M", "MM", "MMM" };
+		 string C[] = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+		 string X[] = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+		 string I[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+		 return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
 	 }
 };
 
