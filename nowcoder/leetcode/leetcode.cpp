186d01387a89967163f1973ccf012a0162637d23
jiawenhao2015 <1490776271@qq.com>
2017-05-27 20:41:31 +0800

    leetcode-374-Guess Number Higher or Lower.txt

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -4218,6 +4218,59 @@ public:
 		 addParenthesis(res, "", 0, 0, n);
 		 return res;
 	 }
+	 /*486. Predict the Winner
+	 Given an array of scores that are non-negative integers. Player 1 picks one of the numbers from either end of the array
+	 followed by the player 2 and then player 1 and so on. Each time a player picks a number, that number will
+	 not be available for the next player. This continues until all the scores have been chosen. The player with the maximum score wins.
+	 Given an array of scores, predict whether player 1 is the winner. You can assume each player plays to maximize his score.
+	 Example 1:
+	 Input: [1, 5, 2]
+	 Output: False
+	 Explanation: Initially, player 1 can choose between 1 and 2.
+	 If he chooses 2 (or 1), then player 2 can choose from 1 (or 2) and 5. If player 2 chooses 5, then player 1 will be left with 1 (or 2).
+	 So, final score of player 1 is 1 + 2 = 3, and player 2 is 5.
+	 Hence, player 1 will never be the winner and you need to return False.
+	 Example 2:
+	 Input: [1, 5, 233, 7]
+	 Output: True
+	 Explanation: Player 1 first chooses 1. Then player 2 have to choose between 5 and 7. No matter which number player 2 choose, player 1 can choose 233.
+	 Finally, player 1 has more score (234) than player 2 (12), so you need to return True representing player1 can win.
+	 Note:
+	 1 <= length of the array <= 20.
+	 Any scores in the given array are non-negative integers and will not exceed 10,000,000.
+	 If the scores of both players are equal, then player 1 is still the winner.*/
+	 bool PredictTheWinner(vector<int>& nums)
+	 {
+
+	 }
+	 /*leetcode-374-Guess Number Higher or Lower
+	 We are playing the Guess Game. The game is as follows:
+	 I pick a number from 1 to n. You have to guess which number I picked.
+	 Every time you guess wrong, I'll tell you whether the number is higher or lower.
+	 You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+	 -1 : My number is lower
+	 1 : My number is higher
+	 0 : Congrats! You got it!
+	 Example:
+	 n = 10, I pick 6.
+	 Return 6.*/
+	 int guess(int num);
+	 int guessNumber(int n)
+	 {
+		 int maxnum = n, minnum = 1;
+		 while (1)
+		 {
+			 int mid = minnum + (maxnum - minnum) / 2;
+			 int gus = guess(mid);
+			 if (gus == 0)return mid;
+			 if (gus == 1)
+			 {
+				 minnum = mid + 1;
+			 }
+			 else maxnum = mid - 1;
+		 }		 
+		  
+	 }
 };
 
 int main()
