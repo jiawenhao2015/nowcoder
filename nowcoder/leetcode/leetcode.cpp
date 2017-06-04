2c364572bce8dc5146b11f6e66d77162f9775ab7
jiawenhao2015 <1490776271@qq.com>
2017-06-04 19:01:19 +0800

    leetcode-34-Search for a Range.txt

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -4416,6 +4416,62 @@ public:
 		 }
 		 return len;
 	 }
+	 /*leetcode-33-Search in Rotated Sorted Array
+	 Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+	 (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+	 You are given a target value to search. If found in the array return its index, otherwise return -1.
+	 You may assume no duplicate exists in the array.*/
+	 int search(vector<int>& nums, int target)
+	 {
+		 int len = nums.size();
+		 if (len == 0)return -1;
+		 for (int i = 0; i < len; i++)
+		 {
+			 if (nums[i] == target)return i;
+		 }
+		 return -1;
+	 }
+	 /*leetcode-34-Search for a Range
+	 Given an array of integers sorted in ascending order, find the starting and ending position of a given target value.
+	 Your algorithm's runtime complexity must be in the order of O(log n).
+	 If the target is not found in the array, return [-1, -1].
+	 For example,
+	 Given [5, 7, 7, 8, 8, 10] and target value 8,
+	 return [3, 4].*/
+	 vector<int> searchRange(vector<int>& nums, int target)
+	 {
+		 int len = nums.size(),low=0,high = len-1,mid = low +(high-low)/2;
+		 vector<int>ret;
+		 while (low <= high)
+		 {
+			 mid = low + (high - low) / 2;
+			 if (nums[mid] == target)
+			 {
+				 while (mid>=0 && nums[mid] == target)
+				 {
+					 mid--;
+				 }
+				 ret.push_back(mid + 1);
+				 mid++;
+				 while (mid<len && nums[mid] == target)
+				 {
+					 mid++;
+				 }
+				 ret.push_back(mid - 1);
+				 break;
+			 }
+			 else if (nums[mid]<target)
+			 {
+				 low = mid + 1;
+			 }
+			 else high = mid - 1;
+		 }
+		 if (ret.size()==0)
+		 {
+			 ret.push_back(-1), ret.push_back(-1);
+		 }
+		 return ret;
+	 }
 };
 
 int main()
