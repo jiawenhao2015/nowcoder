8f506a80d00819b6485ae53d9649fe9ffed931d3
jiawenhao2015 <1490776271@qq.com>
2017-06-05 11:01:21 +0800

    leetcode-51-N-Queens.txt

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -4472,6 +4472,48 @@ public:
 		 }
 		 return ret;
 	 }
+	 /*leetcode-51-N-Queens
+	 */
+	 bool isValid(vector<string>& nQueens, int row, int col, int &n)
+	 {
+		 for (int i = 0; i < row;i++)
+		 {
+			 if (nQueens[i][col] == 'Q')return false;
+		 }
+		 for (int i = row - 1, j = col - 1; i >= 0&&j >= 0;i--,j--)
+		 {
+			 if (nQueens[i][j] == 'Q')return false;
+		 }
+		 for (int i = row - 1, j = col + 1; i >= 0&&j < n;i--,j++)
+		 {
+			 if (nQueens[i][j] == 'Q')return false;
+		 }
+		 return true;
+	 }
+	 void solveNQueens(vector<vector<string>>& ret, vector<string>& nQueens, int row, int & n)
+	 {
+		 if (row == n)
+		 {
+			 ret.push_back(nQueens);
+			 return;
+		 }
+		 for (int col = 0; col < n;col++)
+		 {
+			 if (isValid(nQueens,row,col,n))
+			 {
+				 nQueens[row][col] = 'Q';
+				 solveNQueens(ret, nQueens, row + 1, n);
+				 nQueens[row][col] = '.';
+			 }
+		 }
+	 }
+	 vector<vector<string>> solveNQueens(int n)
+	 {
+		 vector<vector<string>> ret;
+		 vector<string> nQueens(n, string(n, '.'));
+		 solveNQueens(ret, nQueens, 0, n);
+		 return ret;
+	 }
 };
 
 int main()
