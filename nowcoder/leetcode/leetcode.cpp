0f490c05fce379f67b33708ef073798e90c37dec
jiawenhao2015 <1490776271@qq.com>
2017-06-09 21:54:18 +0800

    leetcode-260-Single Number III.txt

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -4638,16 +4638,32 @@ public:
 		 }
 		 return ret;
 	 }
-	 int minMoves(vector<int>& nums)
-	 {
-		 int sum = 0;
-		 for (int i = 0; i < nums.size(); i++)
+	 /*leetcode-260-Single Number III
+	 Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice.
+	 Find the two elements that appear only once.
+	 For example:
+	 Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
+	 Note:
+	 The order of the result is not important. So in the above example, [5, 3] is also correct.
+	 Your algorithm should run in linear runtime complexity.
+	 Could you implement it using only constant space complexity?*/
+	 vector<int> singleNumber(vector<int>& nums) {
+		 int r = 0, n = nums.size(), i = 0, last = 0;
+		 vector<int> ret(2, 0);
+
+		 for (i = 0; i < n; ++i)
+			 r ^= nums[i];
+
+		 last = r & (~(r - 1));
+		 for (i = 0; i < n; ++i)
 		 {
-			 sum += nums[i];
+			 if ((last & nums[i]) != 0)
+				 ret[0] ^= nums[i];
+			 else
+				 ret[1] ^= nums[i];
 		 }
-		 if (nums.size()==0 || sum / nums.size() = nums[0] )
-		 {
-		 }
+
+		 return ret;
 	 }
 };
 
