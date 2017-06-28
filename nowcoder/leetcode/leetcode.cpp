817cd5cc4066fe0f4978a5a8db191874b5c26e8c
jiawenhao2015 <1490776271@qq.com>
2017-06-28 16:04:36 +0800

    leetcode-406-Queue Reconstruction by Height (2).txt

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -3921,21 +3921,7 @@ public:
 		 }
 		 return sum;
 	 }
-	 /* leetcode-406-Queue Reconstruction by Height
-	 Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k),
-	 where h is the height of the person and k is the number of people in front of this person who have a height greater
-	 than or equal to h. Write an algorithm to reconstruct the queue.
-	 Note:
-	 The number of people is less than 1,100.
-	 Example
-	 Input:
-	 [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
-	 Output:
-	 [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]*/
-	 vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people)
-	 {
 
-	 }
 	 /*leetcode-526-Beautiful Arrangement
 	 Suppose you have N integers from 1 to N. We define a beautiful arrangement as an array that is constructed by these N numbers successfully
 	 if one of the following is true for the ith position (1 ¡Ü i ¡Ü N) in this array:
@@ -5170,6 +5156,19 @@ public:
 			// maxdown = max(maxdown, down[i]);
 		 }
 		 return max(up.back(), down.back());
+	 }
+	 /*leetcode-406-Queue Reconstruction by Height*/
+	 vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people)
+	 {
+		 sort(people.begin(), people.end(), [](const pair<int, int>& a, const pair<int, int>& b){
+			 return a.first > b.first || (a.first == b.first && a.second < b.second);
+		 });
+		 vector<pair<int, int>> ret;
+		 for (auto a:people)
+		 {
+			 ret.insert(ret.begin() + a.second, a);
+		 }
+		 return ret;	
 	 }
 };
 
