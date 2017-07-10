d3259170876220e2867225118186a9d62b78adf1
jiawenhao2015 <1490776271@qq.com>
2017-07-10 21:39:08 +0800

    leetcode-89-Gray Code.txt

--- nowcoder/leetcode/leetcode.cpp
+++ nowcoder/leetcode/leetcode.cpp
@@ -5469,6 +5469,22 @@ public:
 		 }
 		 return s;
 	 }
+
+	 /*leetcode-89-Gray Code*/
+	 vector<int> grayCode(int n)
+	 {
+		 vector<int>ret;
+		 ret.push_back(0);
+		 for (int i = 0; i < n;i++)
+		 {
+			 int size = ret.size();
+			 for (int j = size-1; j >=0;j--)
+			 {
+				ret.push_back(ret[j] | 1 << i);
+			 }
+		 }
+		 return ret;
+	 }
 };
 
 int main()
