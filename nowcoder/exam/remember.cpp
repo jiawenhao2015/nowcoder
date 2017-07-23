c47b7cfac92c5e3af921fbdce958f79bf1a70fa9
jiawenhao2015 <1490776271@qq.com>
2017-07-23 13:28:23 +0800

    动态规划

--- nowcoder/exam/remember.cpp
+++ nowcoder/exam/remember.cpp
@@ -374,3 +374,42 @@ double findMaxAverage1(vector<int>& nums, int k)
 		return ret;
 	}
 
+/*[leetcode-646-Maximum Length of Pair Chain]
+
+You are given n pairs of numbers. In every pair, the first number is always smaller than the second number.
+
+Now, we define a pair (c, d) can follow another pair (a, b) if and only if b < c. Chain of pairs can be formed in this fashion.
+
+Given a set of pairs, find the length longest chain which can be formed. You needn't use up all the given pairs. You can select pairs in any order.
+
+Example 1:
+
+Input: [[1,2], [2,3], [3,4]]
+Output: 2
+Explanation: The longest chain is [1,2] -> [3,4]
+ 
+
+Note:
+
+The number of given pairs will be in the range [1, 1000].
+思路：
+动态规划。递推公式是 dp[i] = max(dp[i],dp[j]+1);其中0=<j<i;
+*/
+int findLongestChain(vector<vector<int>>& pairs)
+    {
+        sort(pairs.begin(), pairs.end(), [](const vector<int>&a, const vector<int>&b){return a[0] < b[0]; });
+         
+        vector<int> dp(pairs.size(), 1);
+        for (int i = 1; i < pairs.size(); i++)
+        {
+            for (int j = 0; j<i;j++)
+            {
+                if (pairs[i][0]>pairs[j][1])
+                {
+                    dp[i] = max(dp[i],dp[j]+1);                    
+                }
+            }
+            
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
